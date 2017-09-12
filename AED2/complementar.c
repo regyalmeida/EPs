9ef4faaa43a5dd66879180e648c53ade21cc2e17
regyalmeida <regianynalmeida@gmail.com>
2017-09-12 12:13:47 -0300

    adicionando funcionalidades para gerar grafo complementar (know issue: perde a referencia a partir do 3o verticie)

--- AED2/complementar.c
+++ AED2/complementar.c
@@ -1,6 +1,6 @@
 //
 //  main.c
-//  aed2-xavier-ep1
+//
 //
 //  Created by Regiany Almeida on 03/09/17.
 //  Copyright © 2017 Regiany Almeida. All rights reserved.
@@ -84,6 +84,7 @@ void inserir_aresta(VERTICE* g, int origem, int destino){
     g[origem].inicio = novo;
 }
 
+
 void matriz_para_lista(MATRIZadj *m, VERTICE **g){
     int origem, destino;
     
@@ -111,6 +112,78 @@ void print_full(VERTICE* g, int n){
     printf("\n");
 }
 
+//void excluir_vertice_grafo(VERTICE* g, int exc, int nv){
+//    NO* ant = NULL;
+//    NO* p;
+//    
+//    for(int i=1; i<nv; i++){
+//        p = g[i].inicio;
+//        
+//        while (p && p->prox){
+//            ant = p;
+//            p = p->prox;
+//            if(p->v == exc) {
+//                //printf("entroooou %d, %d \n", p->v, exc);
+//                //printf("ANT %d\n", ant->v);
+//                ant->prox = p->prox;
+//                free(p);
+//            }
+//        }
+//    }
+//}
+
+void excluir_aresta_grafo(VERTICE*g, int a, int b, int nv){
+    
+    NO* p = g[a].inicio;
+    NO* ant = g[a].inicio;
+    
+    for(int i=1; i<=nv; i++){
+        //aux = p;
+        while(p){
+            if(p->v == b){
+                printf("ANT %d\n", ant->v);
+                g[a].inicio = ant;
+                ant->prox = p->prox;
+                free(p);
+            }
+            ant = p;
+            p = p->prox;
+        }
+        
+    }
+    
+}
+
+VERTICE* complementar(VERTICE* g, VERTICE**gComplementar, int nv){
+    NO* p;
+    NO* p2;
+    for(int i=1; i<=nv; i++){
+        p = g[i].inicio;
+        p2 = g[i].inicio;
+        while(p2){
+            if(aresta_existe_grafo(g, i, p2->v)){
+                printf("existeee %d %d \n", i, p2->v);
+                p = p2;
+                excluir_aresta_grafo(*gComplementar, i, p2->v, nv);
+                print_full(*gComplementar, nv);
+            }
+            p2 = p;
+            p2=p2->prox;
+        }
+    }
+    return *gComplementar;
+}
+
+
+VERTICE* preenche_grafo_inteiro(VERTICE *g, int nv){
+    for(int i=1; i<=nv; i++){
+        for(int j=1; j<=nv; j++){
+            inserir_aresta(g, i, j);
+        }
+    }
+    return g;
+}
+
 int main(int argc, char *argv[]){
 
     FILE * arquivo = fopen(argv[1], "r");
@@ -130,20 +203,30 @@ int main(int argc, char *argv[]){
     MATRIZadj* matriz = (MATRIZadj*)malloc(sizeof(MATRIZadj));
     VERTICE *grafo = (VERTICE*)malloc(sizeof(VERTICE) * (numero_vertices+1));
     VERTICE *grafoComplementar = (VERTICE*)malloc(sizeof(VERTICE) * (numero_vertices+1));
+    VERTICE *grafoAuxiliar = (VERTICE*)malloc(sizeof(VERTICE) * (numero_vertices+1));
 
     /* declaracoes */
     inicializa_matriz(matriz, numero_vertices);
     inicializa_grafo(grafo, numero_vertices);
     inicializa_grafo(grafoComplementar, numero_vertices);
+    inicializa_grafo(grafoAuxiliar, numero_vertices);
     
     
-    
+    /* preenchimento da matriz com valores 0 ou 1 */
     for (i = 0; i < (numero_vertices*numero_vertices); i++) {
         fscanf(arquivo, "%d %d %d", &linha, &coluna, &valor);
-        printf("%d, %d, %d \n", linha, coluna, valor);
 
         if(valor==1) inserir_aresta_matriz(matriz, linha, coluna);
-        matriz_para_lista(matriz, &grafo);
-        print_full(grafo, numero_vertices);
     }
+    
+    /* criacao do grafo auxiliar */
+    *grafoAuxiliar = *preenche_grafo_inteiro(grafoAuxiliar, numero_vertices);
+
+    /* conversao da matriz de adj para a lista de adj */
+    matriz_para_lista(matriz, &grafo);
+    print_full(grafo, numero_vertices);
+
+    complementar(grafo, &grafoAuxiliar, numero_vertices);
+    print_full(grafoAuxiliar, numero_vertices);
+    
 }
