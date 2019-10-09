54583e49cdba54eda03c27257000993cee539222
Regiany Almeida <regianynalmeida@gmail.com>
2019-10-08 22:58:46 -0300

    :star: adding comment session, cap6 ok

--- Software Livre/blog/config/routes.rb
+++ Software Livre/blog/config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end 
+  
   get '/articles/new'
 
   root 'welcome#index'
